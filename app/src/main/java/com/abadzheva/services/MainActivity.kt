b56b4dca99e95735868830660489e5e76e761a59
k8abadzheva <make.me.loco@gmail.com>
2024-09-20 01:37:53 +0300

    feat: Add foreground service example
    This commit adds an example of a foreground service.
    
    The `MainActivity` now includes a button to trigger a notification and requests
     the `POST_NOTIFICATIONS` permission if needed.
    
    The `MyService` class remains unchanged.
    This change also adds functions to handle permission requests and open notification settings.

--- app/src/main/java/com/abadzheva/services/MainActivity.kt
+++ app/src/main/java/com/abadzheva/services/MainActivity.kt
@@ -1,37 +1,145 @@
 package com.abadzheva.services
 
-import android.app.Notification
+import android.Manifest
+import android.app.AlertDialog
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.abadzheva.services.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var notificationManager: NotificationManager
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                showNotification()
+                Log.e("MainActivity", "requestPermissionLauncher: isGranted")
+            } else {
+                Log.e("MainActivity", "requestPermissionLauncher: isNOTGranted")
+                showFeatureNeedPermissionDialog()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this, 25))
+            startService(MyService.newIntent(this, 25)) // запускаем сервис
         }
+
         binding.foregroundService.setOnClickListener {
-            showNotification()
+            setupNotification()
+            checkNotificationPermission()
+        }
+    }
+
+    private fun checkNotificationPermission() {
+        val notificationChannel = notificationManager.getNotificationChannel(CHANNEL_ID)
+
+        when {
+            NotificationManagerCompat.from(this).areNotificationsEnabled() -> {
+                if (notificationChannel != null && notificationChannel.importance == NotificationManager.IMPORTANCE_NONE) {
+                    Log.e("MainActivity", "onCreate: Notification channel is disabled by the user.")
+                    showPermissionExplanation()
+                } else {
+                    Log.e("MainActivity", "onCreate: PERMISSION GRANTED")
+                    showNotification()
+                }
+            }
+
+            shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                Log.e("MainActivity", "onCreate: PERMISSION Snackbar.make")
+                showPermissionExplanation()
+            }
+
+            else -> {
+                Log.e("MainActivity", "onCreate: PERMISSION ELSE")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.POST_NOTIFICATIONS,
+                    )
+                } else {
+                    showFeatureNeedPermissionDialog()
+                }
+            }
+        }
+    }
+
+    private fun showPermissionExplanation() {
+        Snackbar
+            .make(binding.root, "Notification blocked", Snackbar.LENGTH_LONG)
+            .setAction("Settings") {
+                openAppNotificationSettings()
+            }.show()
+    }
+
+    private fun showFeatureNeedPermissionDialog() {
+        AlertDialog
+            .Builder(this)
+            .setTitle("Title")
+            .setMessage(
+                "Отсутствует разрешение на показ уведомлений для " +
+                    "${
+                        packageName.substringAfterLast(".").replaceFirstChar { it.uppercase() }
+                    }.\n" +
+                    "Вам не доступна функция ПОКАЗА ВАЖНЫХ УВЕДОМЛЕНИЙ.\n" +
+                    "Нажмите НАСТРОЙКИ, чтобы включить разрешение на показ уведомлений",
+            ).setPositiveButton("Настройки") { _, buttonId ->
+                Log.w("AlertDialog", buttonId.toString() + "was clicked")
+                openAppNotificationSettings()
+            }.setNegativeButton("Отмена", null)
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun setupNotification() {
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT,
+                )
+            notificationManager.createNotificationChannel(notificationChannel)
         }
     }
 
     private fun showNotification() {
         val notification =
-            Notification
-                .Builder(this)
+            NotificationCompat
+                .Builder(this, CHANNEL_ID)
                 .setContentTitle("Title")
                 .setContentText("Text")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .build()
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(1, notification)
+    }
+
+    private fun openAppNotificationSettings() {
+        val intent =
+            Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
